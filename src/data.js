8f51393a97bffc4cef5d2d0caada4f3c899656ef
Yvonne Lin <linyvonnea@gmail.com>
2024-07-25 01:18:18 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
+
+export const pageLinks = [
+    {id:1, href:'#home', text: 'home'},
+    {id:2, href:'#about', text: 'about'},
+    {id:3, href:'#services', text: 'services'},
+    {id:4, href:'#tours', text: 'tours'},
+
+];
+
+export const socialLinks = [
+    {id: 1, href: 'https://www.facebook.com', icon: 'fab fa-facebook'},
+    {id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter'},
+    {id: 3, href: 'https://www.squarespace.com', icon: 'fab fa-squarespace'},
+];
+
+export const services = [
+    {
+        id: 1,
+        icon: 'fas fa-wallet fa-fw',
+        title: 'saving money',
+        text: 'lorem impsum hehehehehehheh. hehe, hedf',
+    },
+    {
+        id: 2,
+        icon: 'fas fa-tree fa-fw',
+        title: 'endless hiking',
+        text: 'lorem yeye ydgsfj ksf',
+    },
+    {
+        id: 3,
+        icon: 'fas fa-socks fa-fw',
+        title: 'amazing comfort',
+        text: 'kuan basta kuan gowd',
+    },
+];
+export const tours = [
+    {id: 1,
+        image:tour1,
+        date: 'aug 26 2024',
+        title: 'Tibet Adventure',
+        info: 'Lorem Ipsum',
+        location: 'china',
+        durations: '6',
+        cost: 'P5',
+    },
+
+    {id: 2,
+        image:tour2,
+        date: 'aug 26 2024',
+        title: 'Tibet Adventure',
+        info: 'Lorem Ipsum',
+        location: 'china',
+        durations: '6',
+        cost: 'P5',
+    },
+
+    {id: 3,
+        image:tour3,
+        date: 'aug 26 2024',
+        title: 'Tibet Adventure',
+        info: 'Lorem Ipsum',
+        location: 'china',
+        durations: '6',
+        cost: 'P5',
+    },
+
+    {id: 4,
+        image:tour4,
+        date: 'aug 26 2024',
+        title: 'Tibet Adventure',
+        info: 'Lorem Ipsum',
+        location: 'china',
+        durations: '6',
+        cost: 'P5',
+    },
+];
